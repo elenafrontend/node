d08366e22b778d985d3a09024c7b92b9a855420a
gumerova.e <gumerova.e@etton.ru>
2021-11-02 16:53:10 +0300

    lesson-5: arguments

---
+++ app.js
@@ -0,0 +1,12 @@
+const arg = process.argv;
+// console.log(arg[2]);
+const a = +arg[2];
+const b = +arg[3];
+console.log(typeof a)
+
+if(a > b) {
+    console.log(a);
+}
+else {
+    console.log(b);>
+}
