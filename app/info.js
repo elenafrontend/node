817373ea9902ede61ad3d365c89af09aa6222ecd
Elena Gumerova <elenafrontend@ya.ru>
2021-11-06 20:25:48 +0300

    rss lesson-6 fs module

--- app/info.js
+++ app/info.js
@@ -1,5 +1,49 @@
-// абсолютный путь к директории файла
-console.log(__dirname);
+// fs module
+const fs = require('fs');
+const path = require('path');
 
-// абсолютный путь к файлу
-console.log(__filename)
+// create directory
+fs.mkdir(path.join(__dirname, 'notes'), err => {
+	if (err) throw err;
+	console.log('Папка была создана')
+})
+
+// create file
+fs.writeFile(
+	path.join(__dirname, 'notes', 'mynotes.txt'),
+	'Hello, Node.js\n',
+	err => {
+		if(err) throw err;
+		console.log('Файл был создан')
+	}
+)
+
+// append file data
+fs.appendFile(
+	path.join(__dirname, 'notes', 'mynotes.txt'),
+	'Append data to file',
+	err => {
+		if(err) throw err;
+		console.log('Файл дополнен')
+	}
+	)
+	
+	// read file
+	fs.readFile(
+	path.join(__dirname, 'notes', 'mynotes.txt'),
+	'utf-8',
+	(err, data) => {
+		if(err) throw err;
+		console.log(data);
+	}
+)
+
+// rename file
+fs.rename(
+	path.join(__dirname, 'notes', 'mynotes.txt'),
+	path.join(__dirname, 'notes', 'notes.txt'),
+	err => {
+		if(err) throw err;
+		console.log('Файл переименован')
+	}
+)
