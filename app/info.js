ceca0e1612353f13880c7266d1b107e473859c2b
Elena Gumerova <elenafrontend@ya.ru>
2021-11-07 13:58:24 +0300

    rss lesson-6 notes app

--- app/info.js
+++ app/info.js
@@ -47,3 +47,17 @@ fs.rename(
 		console.log('Файл переименован')
 	}
 )
+
+// check if file exist
+
+// Do not use fs.access() to check for the accessibility of a file before calling 
+// fs.open(), fs.readFile() or fs.writeFile(). Doing so introduces a race condition, 
+// since other processes may change the file's state between the two calls. 
+// Instead, user code should open/read/write the file directly 
+// and handle the error raised if the file is not accessible.
+
+const file = path.join(__dirname, 'notes', 'mynotes.txt');
+fs.access(file, fs.constants.F_OK,(err) => {
+		console.log(`${file} ${err ? 'does not exist' : 'exist'}`);
+	}
+)
