3c28164a6cf015d6c43f8f39216e7fc91e3ea1f5
Elena Gumerova <elenafrontend@ya.ru>
2021-11-07 14:21:48 +0300

    rss lesson-6 module os

--- app/info.js
+++ app/info.js
@@ -1,63 +1,25 @@
-// fs module
-const fs = require('fs');
-const path = require('path');
+const os = require('os');
 
-// create directory
-fs.mkdir(path.join(__dirname, 'notes'), err => {
-	if (err) throw err;
-	console.log('Папка была создана')
-})
+// Платформа
+console.log(os.platform());
 
-// create file
-fs.writeFile(
-	path.join(__dirname, 'notes', 'mynotes.txt'),
-	'Hello, Node.js\n',
-	err => {
-		if(err) throw err;
-		console.log('Файл был создан')
-	}
-)
+// Архитектура
+console.log(os.arch());
 
-// append file data
-fs.appendFile(
-	path.join(__dirname, 'notes', 'mynotes.txt'),
-	'Append data to file',
-	err => {
-		if(err) throw err;
-		console.log('Файл дополнен')
-	}
-	)
-	
-	// read file
-	fs.readFile(
-	path.join(__dirname, 'notes', 'mynotes.txt'),
-	'utf-8',
-	(err, data) => {
-		if(err) throw err;
-		console.log(data);
-	}
-)
+// Информация о CPU
+console.log(os.cpus());
 
-// rename file
-fs.rename(
-	path.join(__dirname, 'notes', 'mynotes.txt'),
-	path.join(__dirname, 'notes', 'notes.txt'),
-	err => {
-		if(err) throw err;
-		console.log('Файл переименован')
-	}
-)
+// Общий объём памяти
+console.log(os.totalmem());
 
-// check if file exist
+// Объём свободной памяти
+console.log(os.freemem());
 
-// Do not use fs.access() to check for the accessibility of a file before calling 
-// fs.open(), fs.readFile() or fs.writeFile(). Doing so introduces a race condition, 
-// since other processes may change the file's state between the two calls. 
-// Instead, user code should open/read/write the file directly 
-// and handle the error raised if the file is not accessible.
+// Корневая директория
+console.log(os.homedir());
 
-const file = path.join(__dirname, 'notes', 'mynotes.txt');
-fs.access(file, fs.constants.F_OK,(err) => {
-		console.log(`${file} ${err ? 'does not exist' : 'exist'}`);
-	}
-)
+// Время работы системы
+console.log(os.uptime());
+
+// Символ окончания строки в данной системе
+console.log(os.EOL);
